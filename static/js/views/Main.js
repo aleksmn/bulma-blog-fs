09fa1febb9efe5a1a119f0594f465c448eaaa881
aleksmn <al-mn@yandex.ru>
2022-09-15 23:11:39 +0300

    change h1

--- static/js/views/Main.js
+++ static/js/views/Main.js
@@ -8,8 +8,10 @@ export default class extends AbstractView {
 
   async getHtml() {
     return `
-      <h1 class="is-size-1 has-text-primary">Добро пожаловать на главную страницу сайта!</h1>
+      <h1 class="is-size-1 has-text-primary">Веб привет!</h1>
 
+      <p>Быстрый простой и удобрый блог - одностраничное приложение.<p>
+      <p>Сделан при помощи Node, Express и Bulma!<p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum natus quo nobis commodi tempora molestias obcaecati dolores consectetur id ut nemo cumque eum quasi ratione vero, facere, mollitia atque suscipit!</p>
       <p>Aperiam beatae repellendus qui! Eum nulla tempora nisi aperiam earum consectetur ipsa culpa, enim facilis dolores officia id ipsam mollitia praesentium officiis sed ullam pariatur incidunt quos veniam animi atque!</p>
       <p>Adipisci corporis magnam eius quod, nulla exercitationem doloremque incidunt, nesciunt aliquam eveniet temporibus. Ipsam alias doloribus, excepturi, tempora totam, temporibus delectus est natus facilis perspiciatis harum hic dicta ab placeat!</p>
